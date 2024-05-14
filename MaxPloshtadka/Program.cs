8c3c97951ae34074658281ca0012869258bc51a5
churchrat <mihailkrustev08@gmail.com>
2024-05-14 23:10:56 +0300

    coomm1

---
+++ MaxPloshtadka/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace MaxPloshtadka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int[,] @int = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    @int[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            int bestSum = int.MinValue;
+            int bestRow = 0;
+            int bestCol = 0;
+
+            // Намиране на максималната подматрица 2x2
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    int curSum = @int[i, j] + @int[i, j + 1] +
+                                 @int[i + 1, j] + @int[i + 1, j + 1];
+
+                    if (curSum > bestSum)
+                    {
+                        bestSum = curSum;
+                        bestRow = i;
+                        bestCol = j;
+                    }
+                }
+            }
+            Console.WriteLine($"{@int[bestRow, bestCol]} {@int[bestRow, bestCol + 1]}");
+            Console.WriteLine($"{@int[bestRow + 1, bestCol]} {@int[bestRow + 1, bestCol + 1]}");
+        }
+    }
+}
---
+++ PascalPyramid/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PascalPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] triangle = new int[n + 1][];
+            for (int row = 0; row < n; row++)
+            {
+                triangle[row] = new int[row + 1];
+            }
+            triangle[0][0] = 1;
+            for (int row = 0; row < n - 1; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
+                }
+            }
+            int tabs = triangle.Length;
+            for (int row = 0; row < n; row++)
+            {
+                for (int i = 0; i <= tabs; i++)
+                {
+                    Console.Write(" ");
+                }
+                tabs--;
+                for (int col = 0; col <= row; col++)
+                {                  
+                    Console.Write(triangle[row][col] + " ");             
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
